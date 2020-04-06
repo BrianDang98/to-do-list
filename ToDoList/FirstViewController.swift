997410cdd6b1129f8fbada8be34bc1a91601e16b
buichianh <buichianhftu188@gmail.com>
2020-04-06 19:58:01 -0400

    ios project

--- ToDoList/FirstViewController.swift
+++ ToDoList/FirstViewController.swift
@@ -6,13 +6,50 @@
 //  Copyright © 2020 CursedOrca. All rights reserved.
 //
 
+
 import UIKit
 
-class FirstViewController: UIViewController {
+var list = ["Buy milk", "Run 5 miles", "Get Peter", "Plant my new plants"]
 
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    
+    @IBOutlet weak var myTableView: UITableView!
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return (list.count)
+    }
+
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: "cell")
+        cell.textLabel?.text = list[indexPath.row]
+        
+        return(cell)
+    }
+
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath)
+    {
+        if editingStyle == UITableViewCell.EditingStyle.delete
+        {
+            list.remove(at: indexPath.row)
+            myTableView.reloadData()
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        myTableView.reloadData()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
 
 
--- ToDoList/SecondViewController.swift
+++ ToDoList/SecondViewController.swift
@@ -9,12 +9,31 @@
 import UIKit
 
 class SecondViewController: UIViewController {
+    
+    
+    @IBOutlet var input: UITextField!
+    
+    @IBAction func addItem(_ sender: AnyObject)
+    {
+        if (input.text != "")
+        {
+            list.append(input.text!)
+            input.text = ""
+        }
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
 
 
 }
+
 
